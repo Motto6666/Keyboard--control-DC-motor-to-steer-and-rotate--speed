c7d282d1ba4a88e0ec4e0ebad0c2b3e99536630e
=Motto6666 <=790168665@qq.com>
2019-02-27 14:58:47 +0800

    bsp_motor_control.c中时基初始化结构体修改

--- 按键控制直流电机得转向和转速代码demo/BSP/motor-control/bsp_motor_control.c
+++ 按键控制直流电机得转向和转速代码demo/BSP/motor-control/bsp_motor_control.c
@@ -50,13 +50,9 @@ void MOTOR_CONTROL_Config(void)
 	// 自动重装载寄存器的值，累计TIM_Period+1个频率后产生一个更新或者中断
 	TIM_TimeBaseStructure.TIM_Period=GENERAL_TIM_Period;	
 	// 驱动CNT计数器的时钟 = Fck_int/(psc+1)
-	TIM_TimeBaseStructure.TIM_Prescaler= GENERAL_TIM_Prescaler;	
-	// 时钟分频因子 ，配置死区时间时需要用到
-	//TIM_TimeBaseStructure.TIM_ClockDivision=TIM_CKD_DIV1;		
+	TIM_TimeBaseStructure.TIM_Prescaler= GENERAL_TIM_Prescaler;		
 	// 计数器计数模式，设置为向上计数
-	TIM_TimeBaseStructure.TIM_CounterMode=TIM_CounterMode_Up;		
-	// 重复计数器的值，没用到不用管
-	TIM_TimeBaseStructure.TIM_RepetitionCounter=0;	
+	TIM_TimeBaseStructure.TIM_CounterMode=TIM_CounterMode_Up;			
 	// 初始化定时器
 	TIM_TimeBaseInit(GENERAL_TIM, &TIM_TimeBaseStructure);
 
@@ -74,9 +70,9 @@ void MOTOR_CONTROL_Config(void)
 	
 	// 输出比较通道 1
 	TIM_OCInitStructure.TIM_Pulse = Motor_Speed;
-	TIM_OC1Init(GENERAL_TIM, &TIM_OCInitStructure);
+	TIM_OC1Init(GENERAL_TIM, &TIM_OCInitStructure);//配置通道1
 	//使能TIM3自动重装载
-	TIM_OC1PreloadConfig(GENERAL_TIM, TIM_OCPreload_Enable);
+	TIM_OC1PreloadConfig(GENERAL_TIM, TIM_OCPreload_Enable);//配置通道1
 	
 	// 使能计数器
 	TIM_Cmd(GENERAL_TIM, ENABLE);
